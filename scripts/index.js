d8d091e9808abd5acd970b2ca623f067cd55abf8
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-09-08 21:23:11 +0100

    Corrections

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,14 @@
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const currentDate = new Date();
 const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
-const currentUTCTimeMillis = currentDate.getTime(); // Get time in milliseconds
+let currentUTCTimeMillis = currentDate.getMilliseconds(); // Get time in milliseconds
 
 const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
 const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
 
-currentDayElement.textContent = `Current Day: ${currentDayOfWeek}`;
-currentUTCTimeElement.textContent = `Current UTC Time (Milliseconds): ${currentUTCTimeMillis}`;
+currentDayElement.textContent = `${currentDayOfWeek}`;
+currentUTCTimeElement.textContent = `${currentUTCTimeMillis}`;
+
+// const d = new Date();
+
+// document.getElementById("demo").innerHTML = ms;
